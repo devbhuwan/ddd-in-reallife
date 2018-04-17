b0ce9d85f5efea80d8b7cc678e8afd49a176af67
devbhuwan <bhuwan.upadhyay49@gmail.comm>
2018-04-17 23:18:02 +0400

    ddd-in-reallife

--- order/model/src/main/java/io/github/bhuwanupadhyay/ordermanagement/model/Order.java
+++ order/model/src/main/java/io/github/bhuwanupadhyay/ordermanagement/model/Order.java
@@ -4,6 +4,7 @@ import io.github.bhuwanupadhyay.onlinerdersystem.kernel.OrderId;
 import io.github.bhuwanupadhyay.onlinerdersystem.kernel.UserId;
 
 import java.util.LinkedHashSet;
+import java.util.Optional;
 import java.util.Set;
 
 public class Order {
@@ -12,5 +13,12 @@ public class Order {
     private UserId orderBy;
     private Set<OrderLine> orderLines = new LinkedHashSet<>();
     private Set<ChargeLine> chargeLines = new LinkedHashSet<>();
+
+    public void addNewOrderLine(@Nullable OrderLine line) {
+        Optional.ofNullable(line)
+                .ifPresentOrElse(orderLines::add, () -> {
+
+                });
+    }
 
 }
--- order/model/src/test/java/io/github/bhuwanupadhyay/ordermanagement/model/OrderUnitTests.java
+++ order/model/src/test/java/io/github/bhuwanupadhyay/ordermanagement/model/OrderUnitTests.java
@@ -2,13 +2,11 @@ package io.github.bhuwanupadhyay.ordermanagement.model;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class OrderUnitTests {
 
     @Test
     void name() {
         Order order = new Order();
-        assertEquals();
+        order.addNewOrderLine(new OrderLine());
     }
 }
